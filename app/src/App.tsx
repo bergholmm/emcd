3d860c9c02e5782d0b4a7de291d3e548d475b509
Marcus Bergholm <bergholmmarcus@gmail.com>
2020-04-20 18:24:59 +0200

    use material-ui and infinite scroll to render comics

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,28 +1,98 @@
 import React, { useState, useEffect } from 'react'
+import { Theme, createStyles, makeStyles } from '@material-ui/core/styles'
+import {
+  GridList,
+  GridListTile,
+  CssBaseline,
+  Container,
+  GridListTileBar,
+  AppBar,
+  Typography,
+  Toolbar,
+} from '@material-ui/core'
 
-import logo from 'assets/logo.svg'
 import { fetchComics, Comic } from 'api'
+import { useInfiniteScroll } from 'utils/hooks'
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      display: 'flex',
+      flexWrap: 'wrap',
+      justifyContent: 'space-around',
+      overflow: 'hidden',
+      backgroundColor: theme.palette.background.paper,
+      marginTop: '10px',
+    },
+    gridList: {},
+    tile: {
+      backgroundColor: '#fafafa',
+    },
+    titleBar: {
+      background:
+        'linear-gradient(to top, rgba(0,0,0,0.7) 0%, rgba(0,0,0,0.3) 70%, rgba(0,0,0,0) 100%)',
+    },
+  })
+)
 
 const App = () => {
+  const classes = useStyles()
   const [comics, setComics] = useState([] as Comic[])
+  const [nextComic, setNextComic] = useState(null as number | null)
+  const [isFetching, setIsFetching] = useInfiniteScroll(fetchMoreComics)
+
+  async function fetchMoreComics() {
+    const data = await fetchComics(nextComic)
+    setComics([...comics, ...data.comics])
+    setNextComic(data.next)
+    // @ts-ignore
+    setIsFetching(false)
+  }
 
   useEffect(() => {
     const updateComics = async () => {
-      const comics = await fetchComics()
-      setComics(comics)
+      const data = await fetchComics()
+      setNextComic(data.next)
+      setComics(data.comics)
     }
     updateComics()
   }, [])
 
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>EMCD</p>
-        {comics.length &&
-          comics.map(comic => <img src={comic.img} alt='comic' />)}
-      </header>
-    </div>
+    <>
+      <CssBaseline />
+      <AppBar position='fixed'>
+        <Container>
+          <Toolbar>
+            <Typography variant='h6'>EMCD</Typography>
+          </Toolbar>
+        </Container>
+      </AppBar>
+      <Toolbar />
+
+      <Container>
+        <div className={classes.root}>
+          <GridList
+            className={classes.gridList}
+            cellHeight={260}
+            cols={3}
+            spacing={28}>
+            {comics.map(comic => (
+              <GridListTile className={classes.tile} key={comic.img} cols={1}>
+                <img src={comic.img} alt={comic.title} />
+                <GridListTileBar
+                  title={comic.title}
+                  classes={{
+                    root: classes.titleBar,
+                  }}
+                />
+              </GridListTile>
+            ))}
+          </GridList>
+          {isFetching && 'Fetching more comics...'}
+        </div>
+      </Container>
+    </>
   )
 }
 
--- app/src/api/index.ts
+++ app/src/api/index.ts
@@ -12,6 +12,11 @@ export interface Comic {
   day: string
 }
 
+export interface ComicResponse {
+  next: number
+  comics: Comic[]
+}
+
 export const fetchLatestComic = async (): Promise<Comic> => {
   try {
     const response = await fetch('/api/latestComic')
@@ -22,13 +27,16 @@ export const fetchLatestComic = async (): Promise<Comic> => {
   }
 }
 
-export const fetchComics = async (): Promise<Comic[]> => {
+export const fetchComics = async (
+  number?: number | null
+): Promise<ComicResponse> => {
   try {
-    const response = await fetch('/api/comics')
-    const data = await response.json()
-    return data.comics
+    let response
+    if (number) response = await fetch(`/api/comics?number=${number}&limit=21`)
+    else response = await fetch(`/api/comics?limit=21`)
+    return await response.json()
   } catch (error) {
     console.error(error)
-    return []
+    return { next: -1, comics: [] }
   }
 }
